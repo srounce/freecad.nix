485f75e375f9ff0357c78e9530e32b748e5cc4f8
Samuel Rounce <srounce@users.noreply.github.com>
2024-09-08 18:38:48 +0100

    Fix: update-dev-version.sh uses correct package path

--- scripts/update-dev-version.sh
+++ scripts/update-dev-version.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 prefetchJson=$(nix-prefetch-github --json --fetch-submodules FreeCAD FreeCAD)
 
-jq '{ rev: .rev, hash: .hash }' <<< "$prefetchJson" > pkgs/freecad/version.json
+jq '{ rev: .rev, hash: .hash }' <<< "$prefetchJson" > packages/freecad/version.json
