5943da5b2436090cc1f1cd11956c6a1283b29853
Samuel Rounce <me@samuelrounce.co.uk>
2025-06-05 00:23:51 +0100

    feat: Switch to weekly tags instead of daily master

--- scripts/update-dev-version.sh
+++ scripts/update-dev-version.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
-prefetchJson=$(nix-prefetch-github --json --fetch-submodules FreeCAD FreeCAD)
+latestWeekly=$(curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/freecad/freecad/git/refs/tags | jq -r '[.[].ref | select(match("weekly-"))] | sort | last | split("/") | last')
+
+prefetchJson=$(nix-prefetch-github --json --fetch-submodules --rev $latestWeekly FreeCAD FreeCAD)
 
 jq '{ rev: .rev, hash: .hash }' <<< "$prefetchJson" > packages/freecad/version.json
