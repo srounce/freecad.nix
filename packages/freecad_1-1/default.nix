ecd1f8379c2575f421a473ee9e02ee4e0a921013
Samuel Rounce <me@samuelrounce.co.uk>
2026-02-02 23:52:13 +0000

    chore: override nixpkgs freecad for 1.1.0-rc2

--- packages/freecad_1-1/default.nix
+++ packages/freecad_1-1/default.nix
@@ -7,28 +7,9 @@
 }:
 let
   inherit (pkgs)
-    lib
-    stdenv
     fetchFromGitHub
-    cmake
-    pkg-config
-    wrapGAppsHook3
-
-    fmt
-    yaml-cpp
-    xercesc
-    opencascade-occt
-    libGLU
-    vtk
-    medfile
-    hdf5
-    eigen
-    coin3d
-    mpi
-    libspnav
-    libredwg
-    swig
-    doxygen
+    lib
+    gmsh
     ;
 
   versionInfo = builtins.fromJSON (builtins.readFile ./version.json);
@@ -40,91 +21,18 @@ let
     hash = versionInfo.hash;
     fetchSubmodules = true;
   };
-
-  qtVersion = if withQt6 then pkgs.qt6Packages else pkgs.qt5;
-
-  inherit (qtVersion) wrapQtAppsHook;
-
-  qtPackages =
-    (with qtVersion; [
-      qtbase
-      qtsvg
-      qttools
-      qtwayland
-    ])
-    ++ lib.optionals withQt6 [ qtVersion.qt5compat ];
-
-  pythonPackages = with python.pkgs; [
-    boost
-    lark
-    matplotlib
-    pivy
-    pybind11
-    pyside2
-    pyside2-tools
-    shiboken2
-  ];
 in
-stdenv.mkDerivation {
-  pname = "freecad";
-  version = builtins.substring 0 8 versionInfo.version;
-
+pkgs.freecad.overrideAttrs {
   inherit src;
+
+  version = "1.1.0-rc2";
 
   patches = [
     ./patches/0001-NIXOS-don-t-ignore-PYTHONPATH.patch
   ];
 
-  # echo $PYTHONPATH | sed 's/:/\n/g'
-  # exit 1
-  preConfigure = ''
-    qtWrapperArgs+=(--prefix PYTHONPATH : "$PYTHONPATH")
+  postPatch = ''
+    substituteInPlace src/Mod/Fem/femmesh/gmshtools.py \
+      --replace-fail 'self.gmsh_bin = ""' 'self.gmsh_bin = "${lib.getExe gmsh}"'
   '';
-
-  qtWrapperArgs = [
-    "--set COIN_GL_NO_CURRENT_CONTEXT_CHECK 1"
-    "--prefix PATH : ${libredwg}/bin"
-  ] ++ (lib.optional (!withWayland) "--set QT_QPA_PLATFORM xcb");
-
-  postFixup = ''
-    mv $out/share/doc $out
-    ln -s $out/bin/FreeCAD $out/bin/freecad
-    ln -s $out/bin/FreeCADCmd $out/bin/freecadcmd
-  '';
-
-  cmakeFlags = [
-    "-Wno-dev" # turns off warnings which otherwise makes it hard to see what is going on
-    "-DBUILD_DRAWING=ON"
-    "-DINSTALL_TO_SITEPACKAGES=OFF"
-    "-DFREECAD_USE_PYBIND11=ON"
-    "-DBUILD_FLAT_MESH:BOOL=ON"
-  ] ++ (lib.optionals (!withQt6) [ "-DBUILD_QT5=ON" ]);
-
-  nativeBuildInputs = [
-    cmake
-    pkg-config
-    wrapGAppsHook3
-    wrapQtAppsHook
-  ];
-
-  buildInputs = [
-    fmt
-    yaml-cpp
-    xercesc
-    opencascade-occt
-    libGLU
-    vtk
-    medfile
-    hdf5
-    eigen
-    coin3d
-    mpi
-    libspnav
-    swig
-    doxygen
-
-    python
-  ] ++ qtPackages ++ pythonPackages;
-
-  propagatedBuildInputs = pythonPackages;
 }
