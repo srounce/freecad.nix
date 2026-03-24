35fc973d5b4946636e63ada5edae453254729882
Samuel Rounce <me@samuelrounce.co.uk>
2026-03-24 15:36:05 +0000

    fix: freecad-qt6 is now just a ref to the default attr

--- packages/freecad-qt6/default.nix
+++ packages/freecad-qt6/default.nix
@@ -1,3 +1,3 @@
-{ ... }:
-throw "FreeCAD has removed Qt5 support, the default freecad.nix attribute now uses Qt6."
+{ perSystem, ... }:
+builtins.warn "FreeCAD has removed Qt5 support, the default freecad.nix attribute now uses Qt6." perSystem.self.freecad
 
